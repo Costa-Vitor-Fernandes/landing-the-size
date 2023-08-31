666387c6d4e78646899f98d15712db58a71f28b1
VitorCosta <Costa-Vitor-Fernandes>
2023-08-31 15:27:05 -0300

    moves metatag to _document test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,35 +5,6 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <meta id="myViewport" name="viewport" content="width=380"/>
-
-<meta name="twitter:card" content="summary_large_image"/>
-<meta name="twitter:title" content="uSize: Provador Virtual"/>
-<meta name="twitter:description" content="Aprimore a experiência do seu e-commerce de moda com o uSize..."/>
-
-
-<link rel="icon" href="https://usize.vercel.app/favicon.ico" type="image/x-icon"/>
-<link rel="apple-touch-icon" href="https://usize.vercel.app/apple-touch-icon.png"/>
-
-
-<link rel="canonical" href="https://usize.vercel.app"/>
-
-<meta property="og:title" content="uSize: Provador Virtual"/>
-<meta property="og:description" content="Aprimore a experiência do seu e-commerce de moda..."/>
-<meta property="og:image" content="https://usize.vercel.app/_next/image?url=%2FuSizeSymbol.png&w=384&q=75"/>  
-<meta property="og:url" content="https://usize.vercel.app"/>
-<meta property="og:type" content="website"/>
-<meta property="og:locale" content="pt_BR"/>
-<meta property="og:site_name" content="uSize: Provador Virtual"/>
-
-
-<Script id="viewportFunction">{`
-  window.onload = function () {
-    var mvp = document.getElementById('myViewport');
-    mvp.setAttribute('content','width=580');
-}
-  `}
-</Script> 
   <Component {...pageProps} />
   </>
 }
