9e60ed566269a290d70a0ff2ab1f83d10f490483
VitorCosta <Costa-Vitor-Fernandes>
2023-08-31 17:10:59 -0300

    trying to async

--- pages/api/sendEmail.tsx
+++ pages/api/sendEmail.tsx
@@ -1,18 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import sgMail from '@sendgrid/mail'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   if (req.method === "POST") {
-    // const { value } = req.body;
-
     try {
       // const sgMail = require("@sendgrid/mail");
- 
-        sgMail.setApiKey(String(process.env.SENDGRID_API_KEY));
-        console.log('olha a key', process.env.SENDGRID_API_KEY)
+      sgMail.setApiKey(String(process.env.SENDGRID_API_KEY));
+
       const msg = {
         to: 'usize.web@gmail.com', // Change to your recipient
         from: "boodutcivkvqsuykcm@cwmxc.com", // Change to your verified sender
@@ -20,15 +14,10 @@ export default async function handler(
         text: 'client',
         html: `${JSON.stringify(req.body)}`,
       };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-        })
-        .catch((error:any) => {
-          console.error(error);
-        });
 
+      await sgMail.send(msg); // Using await to asynchronously send the email
+
+      console.log("Email sent");
       res.status(200).json({ message: 'Email Sent' });
     } catch (error) {
       console.error(error);
@@ -38,3 +27,5 @@ export default async function handler(
     res.status(405).json({ message: "Method Not Allowed" });
   }
 }
+
+
