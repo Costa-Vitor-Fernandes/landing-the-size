9d5fc858b7176bbf7c5cc5dfe106f593091d3860
VitorCosta <Costa-Vitor-Fernandes>
2023-08-31 14:28:14 -0300

    namechanges

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,16 +12,16 @@ export default function App({ Component, pageProps }: AppProps) {
 <meta name="twitter:description" content="Aprimore a experiência do seu e-commerce de moda com o uSize..."/>
 {/* <meta name="twitter:image" content="https://usize-web.vercel.app/twitter-card-image.png"/> */}
 
-<link rel="icon" href="https://usize-web.vercel.app/favicon.ico" type="image/x-icon"/>
-<link rel="apple-touch-icon" href="https://usize-web.vercel.app/apple-touch-icon.png"/>
+<link rel="icon" href="https://usize.vercel.app/favicon.ico" type="image/x-icon"/>
+<link rel="apple-touch-icon" href="https://usize.vercel.app/apple-touch-icon.png"/>
 
 
-<link rel="canonical" href="https://usize-web.vercel.app"/>
+<link rel="canonical" href="https://usize.vercel.app"/>
 
 <meta property="og:title" content="uSize: Provador Virtual"/>
 <meta property="og:description" content="Aprimore a experiência do seu e-commerce de moda..."/>
-<meta property="og:image" content="https://usize-web.vercel.app/uSizeSymbol.png"/>
-<meta property="og:url" content="https://usize-web.vercel.app"/>
+<meta property="og:image" content="https://usize.vercel.app/uSizeSymbol.png"/>
+<meta property="og:url" content="https://usize.vercel.app"/>
 <meta property="og:type" content="website"/>
 <meta property="og:locale" content="pt_BR"/>
 <meta property="og:site_name" content="uSize: Provador Virtual"/>
